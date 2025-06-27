15d832c1221a669a358301da118b0f840dea6e6c
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2025-06-27 12:54:49 -0400

    rename RichError to error

--- pkg/richerrors/richerros.go
+++ pkg/richerrors/richerros.go
@@ -5,15 +5,15 @@ import (
 	"fmt"
 )
 
-// RichError is an error that contains a code, an external message, and a wrapped error.
-type RichError struct {
+// Error is an error that contains a code, an external message, and a wrapped error.
+type Error struct {
 	Code        int
 	ExternalMsg string
 	Err         error
 }
 
 // Error returns the ExternalMsg if it is set, otherwise it returns the error message of the wrapped error.
-func (e RichError) Error() string {
+func (e Error) Error() string {
 	if e.ExternalMsg != "" {
 		return e.ExternalMsg
 	}
@@ -21,17 +21,17 @@ func (e RichError) Error() string {
 }
 
 // String implements the fmt.Stringer interface.
-func (e RichError) String() string {
+func (e Error) String() string {
 	return e.Error()
 }
 
 // MarshalText implements the encoding.TextMarshaler interface.
-func (e RichError) MarshalText() ([]byte, error) {
+func (e Error) MarshalText() ([]byte, error) {
 	return []byte(e.Error()), nil
 }
 
 // UnmarshalText implements the encoding.TextUnmarshaler interface.
-func (e *RichError) UnmarshalText(text []byte) error {
+func (e *Error) UnmarshalText(text []byte) error {
 	errMsg := string(text)
 	e.ExternalMsg = errMsg
 	e.Err = errors.New(errMsg)
@@ -39,19 +39,19 @@ func (e *RichError) UnmarshalText(text []byte) error {
 }
 
 // Unwrap returns the wrapped error to support errors.Is and errors.As.
-func (e RichError) Unwrap() error {
+func (e Error) Unwrap() error {
 	return e.Err
 }
 
 // Errorf creates a new RichError with the given external message and format.
-func Errorf(externalMsg string, format string, args ...interface{}) RichError {
-	return RichError{
+func Errorf(externalMsg string, format string, args ...interface{}) Error {
+	return Error{
 		ExternalMsg: externalMsg,
 		Err:         fmt.Errorf(format, args...),
 	}
 }
 
-func ErrorWithCodef(code int, externalMsg string, format string, args ...interface{}) RichError {
+func ErrorWithCodef(code int, externalMsg string, format string, args ...interface{}) Error {
 	richErr := Errorf(externalMsg, format, args...)
 	richErr.Code = code
 	return richErr
