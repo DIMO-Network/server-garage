18eabf727c79d239101c01896a8dd56cfdd32136
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2025-08-01 12:22:53 -0400

    fix: remove empty fileNames for env load

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -2,17 +2,21 @@ package env
 
 import (
 	"fmt"
+	"slices"
 
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 )
 
 // LoadSettings is a simple wrapper around godotenv.Load and env.Parse.
-func LoadSettings[T any](filePath ...string) (T, error) {
+func LoadSettings[T any](filePaths ...string) (T, error) {
+	filePaths = slices.DeleteFunc(filePaths, func(file string) bool {
+		return file == ""
+	})
 	var settings T
-	err := godotenv.Load(filePath...)
+	err := godotenv.Load(filePaths...)
 	if err != nil {
-		return settings, fmt.Errorf("failed to load settings from %s: %w", filePath, err)
+		return settings, fmt.Errorf("failed to load settings from %s: %w", filePaths, err)
 	}
 	// Then override with environment variables
 	if err := env.Parse(&settings); err != nil {
